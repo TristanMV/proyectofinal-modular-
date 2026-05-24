9697516e630460ef856014c9ea952612a0048297
TristanMV <tristan.munoz5150@alumnos.udg.mx>
2026-05-24 02:17:58 -0600

    Base de datos actualizada, esquemas de migraciones, constraints y softdeletes

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'price', 'image_path', 'store_id'];
+
+    public function store() 
+    { 
+        return $this->belongsTo(Store::class); 
+    }
+
+    public function categories() 
+    {
+        return $this->belongsToMany(Category::class)->withPivot('assigned_by')->withTimestamps();
+    }
+}
---
+++ database/migrations/2026_05_24_073110_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_id')->constrained()->onDelete('cascade'); // Relación 1:M (30 pts)
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->string('image_path')->nullable(); // Para los archivos (50 pts)
+            $table->softDeletes(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
