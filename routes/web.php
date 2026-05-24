70c644a1d0c3eef2d0f55dc20c127a7aaad39e83
TristanMV <tristan.munoz5150@alumnos.udg.mx>
2026-05-24 11:50:34 -0600

    Integración de mailable personalizado

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Actions\ProductManager; // <-- IMPORTANTE: No olvides importar el componente aquí abajo
+use App\Models\Product;
 
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
+    // Cambiamos la ruta estática por el componente interactivo de Livewire
+    Route::get('/dashboard', ProductManager::class)->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
